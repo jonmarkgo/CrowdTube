8c8eba115b1d153838f5bbcba18ddd04cac7aa74
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-09-11 10:59:14 -0400

    Auth youtube

--- sms.php
+++ sms.php
@@ -10,7 +10,23 @@ $msg = "Sorry, you already submitted a video request. Let someone else have a go
 else {
 	require_once 'Zend/Loader.php'; // the Zend dir must be in your include_path
 Zend_Loader::loadClass('Zend_Gdata_YouTube');
-$yt = new Zend_Gdata_YouTube();
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin'); 
+$authenticationURL= 'https://www.google.com/accounts/ClientLogin';
+$httpClient = 
+  Zend_Gdata_ClientLogin::getHttpClient(
+              $username = 'jonmarkgo@gmail.com',
+              $password = 'zccintwkfsvimeus ',
+              $service = 'youtube',
+              $client = null,
+              $source = 'CrowdTube', // a short string identifying your application
+              $loginToken = null,
+              $loginCaptcha = null,
+              $authenticationURL);
+$developerKey = 'AI39si7mhC6ixzlFGDp6udcIVj1mUGiOMh_BhYOLkGcVPDQshJR9KzSttKm9soC7ooO9FHlKGppgJlxcaPlB0rxucAODJvT00Q';
+$applicationId = 'CrowdTube v1';
+$clientId = 'VHD CrowdTube - v1';
+
+$yt = new Zend_Gdata_YouTube($httpClient, $applicationId, $clientId, $developerKey);
 $searchTerms = $_POST['Body'];
   $yt->setMajorProtocolVersion(2);
   $query = $yt->newVideoQuery();
